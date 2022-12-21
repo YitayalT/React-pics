a8cbf26dfe5aa500b0f098bc45d820d5230a3006
YitayalT <anex518@gmail.com>
2022-12-21 17:46:35 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+
+import { Component } from 'react';
+import axios from "axios";
 import './App.css';
+import SearchBar from './components/SearchBar';
+import ImageList from './components/ImageList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+state = {images: []}
+
+  onSearchSubmit = async (term) => {
+    const response = await axios.get("https://api.unsplash.com//search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID hiKmtT28gvuRry3zqFX0oY75jIv--mMITwpAm_2n4oI",
+      },
+    });
+    this.setState({images:response.data.results});
+
+  };
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        <ImageList images={ this.state.images} />
+      </div>
+    );
+  }
 }
-
 export default App;
